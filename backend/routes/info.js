0ecb5c36b43cc2598c220b74a68ed09a7d322a98
denyshubh <denyshubham@gmail.com>
2019-08-29 02:03:01 +0530

    backend v1.0.1

--- backend/routes/info.js
+++ backend/routes/info.js
@@ -4,8 +4,20 @@ const Events = require('../models/info')
 
 router.get('/', async (req, res) => {
 	let result = await Events.findInfo(req.query.page)
-	if (!result) return res.status(404).send('404 No response from server');
-	console.log('Data Received From Server is', JSON.parse(result));
+	if (!result) return res.status(404).send('404 No response from server')
+	console.log('Data Received From Server is', JSON.parse(result))
+	res.status(200).send(result)
+})
+
+router.get('/client/:id', async (req, res) => {
+	let result = await Events.findAll({
+		where: {
+			care_recipient_id: req.params.id
+		}
+	})
+	if (!result) {
+		return res.status(404).send('404 Client Not Found');
+	}
 	res.status(200).send(result)
 })
 
